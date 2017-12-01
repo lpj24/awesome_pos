e9bc4c8a61a669c039dae8a73cc3a6893e441d32
lpj24 <lipenju24@163.com>
2017-12-01 11:50:17 +0800

    'add'

---
+++ src/components/page/pos.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="pos">
+        <div>
+            <el-row>
+                <el-col :span="7" class="pos-order" id="order-list">
+                    点餐
+                </el-col>
+
+                <el-col :span="17">
+                    商品展示
+                </el-col>
+            </el-row>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Pos",
+        data() {
+            return {}
+        },
+        mounted:function(){
+            var orderHeight=document.body.clientHeight;
+            document.getElementById("order-list").style.height=orderHeight+'px';
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
